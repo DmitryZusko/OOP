1289508e9731a4e6b4e43b7cdde89cb63d980b63
Dmitry <elxf123@gmail.com>
2022-07-10 18:07:05 +0300

    Added Task_3

---
+++ Task_3/Dispose.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+// here ara all Dispose methods
+namespace test
+{
+    partial class Matrix
+    {
+        protected virtual void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                Console.WriteLine($"\nI dunno what I should clean here with matrix\n{Print()}");
+            }
+        }
+
+        public void Dispose()
+        {
+            if (isDisposed)
+            {
+                return;
+            }
+            Dispose(true);
+            GC.SuppressFinalize(this);
+            isDisposed = true;
+        }
+    }
+}
