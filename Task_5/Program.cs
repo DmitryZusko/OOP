c21d6925cce5e102a21d67b97b74ed8b4c73749b
Dmitry <elxf123@gmail.com>
2022-07-14 21:25:21 +0300

    Task 5 is ready

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -10,7 +10,22 @@ namespace Task_5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cNum1 = new ComplexNumber { Real = 1, Imaginary = 1 };
+            var cNum2 = new ComplexNumber { Real = -2, Imaginary = -1 };
+            var gNum1 = new GNumber<ComplexNumber> { Number = cNum1 };
+            var gNum2 = new GNumber<ComplexNumber> { Number = cNum2 };
+            var gNum3 = gNum1 + gNum2;
+            var gNum4 = gNum1 - gNum2;
+            var gNum5 = gNum1 * gNum2;
+            Console.WriteLine($"First number: {gNum1.Print()},\tsecond number: {gNum2.Print()}");
+            Console.WriteLine("Sum = " + gNum3.Print());
+            Console.WriteLine("Substraction = " + gNum4.Print());
+            Console.WriteLine("Multiplication = " + gNum5.Print());
+            Console.WriteLine("Num1 > Num2 : " + (gNum1 > gNum2));
+            Console.WriteLine("Num1 < Num2 : " + (gNum1 < gNum2));
+            Console.WriteLine("Num1 == Num2 : " + (gNum1 == gNum2));
+            Console.WriteLine("Num1 != Num2 : " + (gNum1 != gNum2));
+
         }
     }
 }
